caccef0af38b781e76616030bd1c8d0c9e712706
StefanYYC <stefanspeter26@gmail.com>
2021-03-18 15:14:09 +0100

    slider hp

---
+++ js/slide.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".wrapper-carousel").slick({
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerPadding: '60px',
+
+    arrows: false,
+    focusOnSelect: true,
+    cssEase: "linear",
+   // autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
